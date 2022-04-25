56f67264a1706ce3f1045017b7e386f1b9cee3f6
JhonCuri97 <50054936+JhonCuri97@users.noreply.github.com>
2022-04-25 09:07:35 -0500

    Proy django

---
+++ webempresa/blog/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.utils.timezone import now
+from django.contrib.auth.models import User
+# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.name
+    
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    published = models.DateTimeField(default=now)
+    image = models.ImageField(upload_to="blog", null=True, blank=True)
+    author = models.ForeignKey(User, on_delete=models.PROTECT)
+    categories = models.ManyToManyField(Category, related_name="get_posts")
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.title
